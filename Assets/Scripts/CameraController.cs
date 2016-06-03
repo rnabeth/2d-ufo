b843bba626155a81b94640ac27fe79c38d126e41
Rafael Nabeth <rafaelnabeth@gmail.com>
2016-06-03 23:53:57 +0100

    Following the Player with the Camera and Picking Up Collectables Objects

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start ()
+	{
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Better for follow cameras, procedural animation and ghathering last know states
+	void LateUpdate ()
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
